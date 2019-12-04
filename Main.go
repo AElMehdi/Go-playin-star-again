937230c367f9e0b7440f102001fa6fc7b9f184a7
AElMehdi <assirelmehdi@gmail.com>
2019-12-04 20:48:30 +0100

    Return multiple results from a funcation

--- Main.go
+++ Main.go
@@ -7,6 +7,7 @@ import (
 func main()  {
 	create("hello", false, true, 1, 2,3)
 	fmt.Println("The sum is", add(2, 3))
+	fmt.Println(swap("first", "last"))
 }
 
 func add(x, y int) int {
@@ -14,4 +15,8 @@ func add(x, y int) int {
 }
 func create(s string, b, b1 bool, i, i2, i3 int ) {
 	fmt.Println(s, b, b1, i, i2, i3)
+}
+
+func swap(x string, y string) (string, string) {
+	return y, x
 }
