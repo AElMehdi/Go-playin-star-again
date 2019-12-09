65d0ca922f181cb53df9dd1ff7d3c504b5a8acb7
AElMehdi <assirelmehdi@gmail.com>
2019-12-09 21:51:10 +0100

    For loop continued
    For loop without optional statements

--- FlowControl.go
+++ FlowControl.go
@@ -2,12 +2,21 @@ package main
 
 func main() {
 	forLoop()
+	forLoopContinued()
 }
 
 func forLoop() {
 	sum := 0
 	for i := 0; i < 10; i++ {
 		sum += i
+	}
+	println(sum)
+}
+
+func forLoopContinued() {
+	sum := 1
+	for ; sum < 1000; {
+		sum += sum
 	}
 	println(sum)
 }
