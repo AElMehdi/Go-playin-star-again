127e6e37fc2288dd83e8a3a41798be686871083c
AElMehdi <assirelmehdi@gmail.com>
2019-12-10 19:48:54 +0100

    If construct

--- FlowControl.go
+++ FlowControl.go
@@ -1,10 +1,16 @@
 package main
 
+import (
+	"fmt"
+	"math"
+)
+
 func main() {
 	forLoop()
 	forLoopContinued()
 	whileLoop()
-	forever()
+	//forever()
+	fmt.Println(sqrt(2), sqrt(-4))
 }
 
 func forLoop() {
@@ -34,4 +40,11 @@ func whileLoop() {
 func forever() {
 	for {
 	}
+}
+
+func sqrt(x float64) string {
+	if x < 0 {
+		return sqrt(-x) + "i"
+	}
+	return fmt.Sprint(math.Sqrt(x))
 }
