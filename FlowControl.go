a65d5e34fd559d435b67142f89d87e5d35412339
AElMehdi <assirelmehdi@gmail.com>
2019-12-13 15:49:16 -0800

    Switch case example

--- FlowControl.go
+++ FlowControl.go
@@ -3,19 +3,21 @@ package main
 import (
 	"fmt"
 	"math"
+	"runtime"
 )
 
-//func main() {
-//	forLoop()
-//	forLoopContinued()
-//	whileLoop()
-//	//forever()
-//	fmt.Println(sqrt(2), sqrt(-4))
-//	fmt.Println(
-//		pow(3, 2, 10),
-//		pow(3, 3, 20),
-//		)
-//}
+func main() {
+	//forLoop()
+	//forLoopContinued()
+	//whileLoop()
+	////forever()
+	//fmt.Println(sqrt(2), sqrt(-4))
+	//fmt.Println(
+	//	pow(3, 2, 10),
+	//	pow(3, 3, 20),
+	//	)
+	switchCase()
+}
 
 func forLoop() {
 	sum := 0
@@ -41,17 +43,17 @@ func whileLoop() {
 	println(sum)
 }
 
-//func forever() {
-//	for {
-//	}
-//}
-
 func sqrt(x float64) string {
 	if x < 0 {
 		return sqrt(-x) + "i"
 	}
 	return fmt.Sprint(math.Sqrt(x))
 }
+
+//func forever() {
+//	for {
+//	}
+//}
 
 func pow(x, n, limit float64) float64 {
 	if v := math.Pow(x, n); v < limit {
@@ -60,4 +62,16 @@ func pow(x, n, limit float64) float64 {
 		fmt.Printf("%g >= %g \n", v, limit)
 	}
 	return limit
+}
+
+func switchCase() {
+	fmt.Print("Go runs on ")
+	switch os := runtime.GOOS; os {
+	case "darwin":
+		fmt.Println("OS X.")
+	case "linux":
+		fmt.Println("Linux.")
+	default:
+		fmt.Printf("%s. \n", os)
+	}
 }
