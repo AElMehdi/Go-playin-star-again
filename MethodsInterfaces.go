331ad935f3cae2c2988878dd082c65983d060538
AElMehdi <assirelmehdi@gmail.com>
2020-01-01 20:18:44 +0100

    Methods are just functions with a receiver

--- MethodsInterfaces.go
+++ MethodsInterfaces.go
@@ -15,7 +15,12 @@ func (u User) isAdult() bool {
 	return u.age >= 18
 }
 
+func isAdult(user User) bool {
+	return user.age >= 18
+}
+
 func methods() {
 	anAdult := User{"Dean", 45,}
 	fmt.Println(anAdult.isAdult())
+	fmt.Println(isAdult(anAdult))
 }
