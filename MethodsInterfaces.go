157e4989f00ef21b02cac476122e2cef51221535
AElMehdi <assirelmehdi@gmail.com>
2020-01-02 22:44:11 +0100

    Method's pointer receivers

--- MethodsInterfaces.go
+++ MethodsInterfaces.go
@@ -4,17 +4,22 @@ import "fmt"
 
 func main() {
 	methods()
+	pointerReceivers()
 }
 
 type MyInt int
 
 type User struct {
 	name string
-	age int
+	age  int
 }
 
 func (u User) isAdult() bool {
 	return u.age >= 18
+}
+
+func (u *User) setAge() {
+	u.age = u.age + 18
 }
 
 func (age MyInt) isAdult() bool {
@@ -29,7 +34,14 @@ func methods() {
 	anAdult := User{"Dean", 45,}
 	fmt.Println(anAdult.isAdult())
 	fmt.Println(isAdult(anAdult))
-	
+
 	anAge := MyInt(13)
 	fmt.Println(anAge.isAdult())
+}
+
+func pointerReceivers() {
+	youssef := User{"Youssef", 15}
+	fmt.Println(youssef.isAdult())
+	youssef.setAge()
+	fmt.Println(youssef.isAdult())
 }
