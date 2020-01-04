ae5e85aa44459f8b98313891cf038d67c88d6b2f
AElMehdi <assirelmehdi@gmail.com>
2020-01-04 22:27:49 +0100

    Interfaces implementation

--- MethodsInterfaces.go
+++ MethodsInterfaces.go
@@ -1,10 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
 	methods()
 	pointerReceivers()
+	interfaces()
 }
 
 type MyInt int
@@ -44,4 +48,44 @@ func pointerReceivers() {
 	fmt.Println(youssef.isAdult())
 	setAge(&youssef, 3)
 	fmt.Println(youssef.isAdult())
+}
+
+// Interfaces
+// Abser interface
+type Abser interface {
+	abs() float64
+}
+
+// A type MyVertex: Implements Abser
+type MyVertex struct {
+	x, y float64
+}
+
+func (v *MyVertex) abs() float64 {
+	fmt.Println("Vertex called")
+	return math.Sqrt(v.x*v.x + v.y*v.y)
+}
+
+// Float wrapper
+type MyFloat float64
+
+func (f MyFloat) abs() float64 {
+	fmt.Println("MyFloat called")
+	if f < 0 {
+		return float64(-f)
+	}
+	return float64(f)
+}
+
+func interfaces() {
+	var a Abser
+	f := MyFloat(-math.Sqrt2)
+	v := MyVertex{3, 4,}
+
+	// Make MyFloat implements the interface Abser
+	a = f
+	// Make *MyVertex implements the interface Abser
+	a = &v
+
+	fmt.Println(a.abs())
 }
