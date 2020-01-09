81e661720f117f4755364e85ee5e3c7add55d322
AElMehdi <assirelmehdi@gmail.com>
2020-01-09 18:41:29 +0100

    Type siwtches

--- MethodsInterfaces.go
+++ MethodsInterfaces.go
@@ -12,6 +12,7 @@ func main() {
 	noImplicitImplementation()
 	emptyInterface()
 	typeAssertions()
+	typeSwitches()
 }
 
 type MyInt int
@@ -173,4 +174,21 @@ func typeAssertions() {
 
 	//f = i.(float64) // panic
 	fmt.Println(f)
+}
+
+func typeSwitches() {
+	do(21)
+	do("Wa feen")
+	do(true)
+}
+
+func do(i interface{}) {
+	switch v := i.(type) {
+	case int:
+		fmt.Printf("Twice %v is %v\n", v, v)
+	case string:
+		fmt.Printf("%q is %v bytes long\n", v, len(v))
+	default:
+		fmt.Printf("I don't know about type %T\n", v)
+	}
 }
