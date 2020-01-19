c91c4729af0748b7572f69d2e289caaf199b4a28
AElMehdi <assirelmehdi@gmail.com>
2020-01-19 20:08:34 +0100

    Channels with goroutines

--- Concurrency.go
+++ Concurrency.go
@@ -7,6 +7,7 @@ import (
 
 func main() {
 	goroutines()
+	channels()
 }
 
 func goroutines() {
@@ -19,4 +20,25 @@ func say(word string) {
 		time.Sleep(100 * time.Millisecond)
 		fmt.Println(word)
 	}
+}
+
+func channels() {
+	s := []int{7, 2, 8, -9, 4, 0}
+
+	c := make(chan int)
+
+	go sum(s[:len(s)/2], c)
+	go sum(s[len(s)/2:], c)
+
+	x, y := <-c, <-c // receive from c
+
+	fmt.Println(x, y, x+y)
+}
+
+func sum(s []int, c chan int) {
+	sum := 0
+	for _,v := range s {
+		sum += v
+	}
+	c <- sum
 }
