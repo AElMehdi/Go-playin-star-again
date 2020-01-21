23835e5e5d17995949dd885a74a89d02e7798edb
AElMehdi <assirelmehdi@gmail.com>
2020-01-21 17:44:22 +0100

    Range and closing a channel

--- Concurrency.go
+++ Concurrency.go
@@ -9,6 +9,7 @@ func main() {
 	goroutines()
 	channels()
 	bufferedChannels()
+	rangeAndClose()
 }
 
 func goroutines() {
@@ -38,7 +39,7 @@ func channels() {
 
 func sum(s []int, c chan int) {
 	sum := 0
-	for _,v := range s {
+	for _, v := range s {
 		sum += v
 	}
 	c <- sum
@@ -50,4 +51,21 @@ func bufferedChannels() {
 	c <- 2
 	fmt.Println(<-c)
 	fmt.Println(<-c)
+}
+
+func rangeAndClose() {
+	c := make(chan int, 10)
+	go fibonacci(cap(c), c)
+	for i := range c {
+		fmt.Println(i)
+	}
+}
+
+func fibonacci(n int, c chan int) {
+	x, y := 0, 1
+	for i := 0; i < n; i++ {
+		c <- x
+		x, y = y, x+y
+	}
+	close(c)
 }
