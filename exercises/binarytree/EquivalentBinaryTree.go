8b56ee551349729677f0f4adb35b6c7ddcfbee38
AElMehdi <assirelmehdi@gmail.com>
2020-01-25 20:27:29 +0100

    Exercise: Final solution

--- exercises/binarytree/EquivalentBinaryTree.go
+++ exercises/binarytree/EquivalentBinaryTree.go
@@ -22,24 +22,40 @@ func recursiveWalker(t *tree.Tree, ch chan int) {
 
 // Check if the values sent are the same
 func Same(t1, t2 *tree.Tree) bool {
-	return false
-}
+	firstChan, secondChan := make(chan int), make(chan int)
 
-func main() {
-	aTree := tree.New(1)
-	aChannel := make(chan int)
+	go Walk(t1, firstChan)
+	go Walk(t2, secondChan)
 
-	go Walk(aTree, aChannel)
+	for {
+		x1, ok1 := <-firstChan
+		x2, ok2 := <-secondChan
 
-	for v := range aChannel {
-		fmt.Println(v)
+		if areChannelsEmpty(ok1, ok2) {
+			return haveSameSize(ok1, ok2)
+		}
+		if x1 != x2 {
+			return false
+		}
 	}
 }
 
+func haveSameSize(ok1 bool, ok2 bool) bool {
+	return ok1 == ok2
+}
+
+func areChannelsEmpty(ok1 bool, ok2 bool) bool {
+	return !ok1 || !ok2
+}
+
+func main() {
+	fmt.Println(Same(tree.New(1), tree.New(1)))
+	fmt.Println(Same(tree.New(10), tree.New(1)))
+}
 
 // Steps to implement the logic:
 /*
 1- Walk a single tree DONE
 2- Print the values from the channel passed to the Walk function DONE
-3- Implement the Same function which will just compare two tree, through the values passed in the channels!
+3- Implement the Same function which will just compare two tree, through the values passed in the channels! Done
 */
